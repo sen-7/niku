4fa34f098b94e00fc7972bfb9c439b1e356e611f
Sen-shichi <sen-7@dummy.mail>
2017-03-06 18:02:57 -0500

    add contour

--- niku.py
+++ niku.py
@@ -7,9 +7,9 @@ from matplotlib.colors import LinearSegmentedColormap
 
 def main():
   ch = 1.1e-7 # m2s-1
-  t0 = float(input("Initial temperature [C] : ") or "10")
-  t1 = float(input("Boundary temperature [C] : ") or "100")
-  xmax = float(input("Thickness [cm] : ") or "1") * 0.005
+  t0 = float(input("Initial temperature [10 C] : ") or "10")
+  t1 = float(input("Boundary temperature [75 C] : ") or "75")
+  xmax = float(input("Thickness [4 cm] : ") or "4") * 0.005
   nsec = 100.0 * 2.5 * (xmax / 0.005)**2
   dt = 0.01 * (xmax / 0.005)**2
   dx = xmax / 25
@@ -33,7 +33,7 @@ def main():
     dtmp[nx-1] = ch * dt * (tmp[nx-1] - 2.0 * tmp[nx-1] + tmp[nx-2]) / (dx * dx)
     tmp[:] = tmp[:] + dtmp[:]
 
-  os.system("convert -delay 10 -loop 0 ./img/*.png ./img/all.gif")
+  os.system("convert -delay 15 -loop 0 ./img/*.png ./img/all.gif")
   os.system("rm -f img/*.png")
 
 def plot_snap(tmp, i, nx, xmax, dt, t0, t1):
@@ -51,15 +51,23 @@ def plot_snap(tmp, i, nx, xmax, dt, t0, t1):
                       (1.0, 0.4, 0.4)) }
   mycm = LinearSegmentedColormap('mycm', cdict1)
 
-  data = np.zeros((nx*2,1))
-  data[0:nx,   0] = tmp
-  data[nx:2*nx,0] = tmp[::-1]
+  xdim = 10
+  data = np.zeros((nx*2,xdim))
+  for k in range(xdim):
+    data[0:nx,   k] = tmp
+    data[nx:2*nx,k] = tmp[::-1]
   fig, ax = plt.subplots(1)
-  map1 = ax.pcolor(data, cmap=mycm)
+  map1 = ax.imshow(data, cmap=mycm, interpolation="bilinear", aspect="auto")
   map1.set_clim(0, 100.0)
   plt.tick_params(axis="x", which="both", bottom="off", top="off", labelbottom="off")
   plt.tick_params(axis="y", which="both", left="off", right="off", labelleft="off")
   cbar = plt.colorbar(map1)
+
+  levels = np.arange(0, 100, 10)
+  extent = (-0.5, xdim-0.5, 0, nx*2-1)
+  map2 = ax.contour(data, levels, colors="w", extent=extent) # , origin="lower")
+  ax.clabel(map2, fmt="%02d", colors="w")
+
   plt.title("[%d cm, init %i deg, boundary %i deg] t = %isec" % (xmax*2*100, t0, t1, i*dt))
   plt.savefig("./img/%06i.png" % i)
   plt.close()
